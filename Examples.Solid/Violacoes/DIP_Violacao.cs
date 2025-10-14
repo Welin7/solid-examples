3aca5fa011576da9d54c7926091e58905b37b023
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-10-13 21:52:29 -0300

    Correções no DIP.

--- Examples.Solid/Violacoes/DIP_Violacao.cs
+++ Examples.Solid/Violacoes/DIP_Violacao.cs
@@ -30,8 +30,8 @@ namespace SolidExamples.Violacoes
             Console.WriteLine("\n[DIP - Violação - Inversão de Dependência]");
             var notificador = new Notificador();
             notificador.Notificar("Mensagem via Email");
-            Console.WriteLine("Tentando mudar para SMS...");
-
+            Console.WriteLine("");
+            
             Console.WriteLine("Importante, O ponto é que a violação não quebra o programa imediatamente, ele roda normalmente.");
             Console.WriteLine("No entanto, quebra a flexibilidade ou extensibilidade.");
             Console.WriteLine("");
